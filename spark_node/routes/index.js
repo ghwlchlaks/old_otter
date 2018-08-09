2013f4eaf47df794abd1ec370bbeb078b19dd1d6
ghwlchlaks <ghwlchlaks@naver.com>
2018-08-09 14:03:31 +0900

    yarn running done list 구별 ver_0_1

--- spark_node/routes/index.js
+++ spark_node/routes/index.js
@@ -8,7 +8,7 @@ var fs = require('fs')
 const SparkPolicy = require('../policies/SparkPolicy')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express'});  
+	res.render('index1', { title: 'Express'});  
 });
 
 router.post('/sparkSubmit', SparkPolicy.sparkSubmit )
