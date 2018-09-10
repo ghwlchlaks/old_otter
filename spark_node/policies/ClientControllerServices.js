11d610f25bdcdeea02948f12dbf6fb457c2e3203
4qazwsx <4qazwsx@naver.com>
2018-09-10 15:04:35 +0900

    log4j 자동 수정 추가

--- spark_node/policies/ClientControllerServices.js
+++ spark_node/policies/ClientControllerServices.js
@@ -117,22 +117,43 @@ module.exports = {
 		})
 	},
 	sparkLog(req, res){
-		//first, make log file to log4j
 		fs.readFile('/var/log/spark.log', 'utf-8', function(err, data){
 
-			var latelyLog = new Array()
-			latelyLog[0] = ''
-			var visibleLog = 20
+			var addLog4j = "log4j.rootLogger=${root.logger}\nlog4j.appender.RollingAppender=org.apache.log4j.DailyRollingFileAppender\nlog4j.appender.RollingAppender.File=/var/log/spark.log\nlog4j.appender.RollingAppender.DatePattern='.'yyyy-MM-dd\nlog4j.appender.RollingAppender.layout=org.apache.log4j.PatternLayout\nlog4j.appender.RollingAppender.layout.ConversionPattern=[%p] %d %c %M - %m%n\n# By default, everything goes to console and file\nlog4j.rootLogger=INFO, RollingAppender\nroot.logger=INFO,console\nlog4j.appender.console=org.apache.log4j.ConsoleAppender\nlog4j.appender.console.target=System.err\nlog4j.appender.console.layout=org.apache.log4j.PatternLayout\nlog4j.appender.console.layout.ConversionPattern=%d{yy/MM/dd HH:mm:ss} %p %c{2}: %m%n\nshell.log.level=WARN\nlog4j.logger.org.eclipse.jetty=WARN\nlog4j.logger.org.spark-project.jetty=WARN\nlog4j.logger.org.spark-project.jetty.util.component.AbstractLifeCycle=ERROR\nlog4j.logger.org.apache.spark.repl.SparkIMain$exprTyper=INFO\nlog4j.logger.org.apache.spark.repl.SparkILoop$SparkILoopInterpreter=INFO\nlog4j.logger.org.apache.parquet=ERROR\nlog4j.logger.parquet=ERROR\nlog4j.logger.org.apache.hadoop.hive.metastore.RetryingHMSHandler=FATAL\nlog4j.logger.org.apache.hadoop.hive.ql.exec.FunctionRegistry=ERROR\nlog4j.logger.org.apache.spark.repl.Main=${shell.log.level}\nlog4j.logger.org.apache.spark.api.python.PythonGatewayServer=${shell.log.level}\nlog4j.logger.org.apache.spark.repl.Main=${shell.log.level}\nlog4j.logger.org.apache.spark.api.python.PythonGatewayServer=${shell.log.level}"
 
-			data = data.split('\n')
+			if(err){//읽기에 오류 발생시
 
-			for(i = data.length-2 ; i > data.length-visibleLog ; i--){
-				for(j = visibleLog+2 ; j > -1 ; j--){
-					latelyLog[j] = latelyLog[j] + data[i] + '\n'
+				//log4j 파일 수정
+				fs.writeFile('/opt/cloudera/parcels/CDH/lib/spark/conf/log4j.properties',addLog4j,'utf-8',function(err){
+					if(err){
+						console.log('first, install spark with Cloudera Manager')
+					} else {
+						console.log('revise log4j')
+					}
+				})
+
+				//로그 파일 생성
+				fs.writeFile('/var/log/spark.log','make new log file','utf-8',function(err){
+					if(err){
+					} else {
+						console.log('make new log file')
+					}
+				})
+			
+			} else {
+				var latelyLog = new Array()
+				latelyLog[0] = ''
+				var visibleLog = 20
+
+				data = data.split('\n')
+
+				for(i = data.length-2 ; i > data.length-visibleLog ; i--){
+					for(j = visibleLog+2 ; j > -1 ; j--){
+						latelyLog[j] = latelyLog[j] + data[i] + '\n'
+					}
 				}
+				res.send({latelyLog : latelyLog, allLog : data})
 			}
-
-			res.send({latelyLog : latelyLog, allLog : data})
 		})
 	}
 }
