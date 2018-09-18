8293b882d368f2018f5f3b184073057875a62260
ghwlchlaks <ghwlchlaks@naver.com>
2018-09-18 14:33:26 +0900

    작업 폴더 트리 구성을 위한 불필요한 파일 삭제

--- spark_node/customRoutes/inbound.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/inbound', function(req, res, next) {
-  res.render('inbound', { title: 'Express' });
-});
-
-module.exports = router;
--- spark_node/customRoutes/outbound.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/outbound', function(req, res, next) {
-  res.render('outbound', { title: 'Express' });
-});
-
-module.exports = router;
